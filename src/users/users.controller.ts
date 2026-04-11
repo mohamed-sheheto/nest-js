6882265cf798c0c5943db7ba4eb6070eaf5755f4
Mohamed Sheheto <113710619+mohamed-sheheto@users.noreply.github.com.>
2026-04-11 04:25:20 +0200

    use service for business logic and controllers only for routing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,38 +8,31 @@ import {
   Body,
   HttpCode,
   HttpStatus,
-  NotFoundException,
   ParseUUIDPipe,
 } from '@nestjs/common';
-import { v4 as uuid } from 'uuid';
 import { createUserDto } from './dtos/create-user.dto';
 import { updateUserDto } from './dtos/update-user.dto';
 import { UserEntity } from './user.entity';
+import { UserService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UserService) {}
+
   private readonly users: UserEntity[] = [];
   @Get()
   findAll(): UserEntity[] {
-    return this.users;
+    return this.userService.findUsers();
   }
 
   @Get(':id')
   findOne(@Param('id', ParseUUIDPipe) id: string): UserEntity {
-    const user = this.users.find((user) => user.id === id);
-    if (!user) throw new NotFoundException('user not found, please try again ');
-
-    return user;
+    return this.userService.findUserById(id);
   }
 
   @Post()
   create(@Body() createUserDto: createUserDto): UserEntity {
-    const newUser = {
-      ...createUserDto,
-      id: uuid(),
-    };
-    this.users.push(newUser);
-    return newUser;
+    return this.userService.createUser(createUserDto);
   }
 
   @Patch(':id')
@@ -47,16 +40,12 @@ export class UsersController {
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateUserDto: updateUserDto,
   ): UserEntity {
-    const index = this.users.findIndex((user) => user.id === id);
-    return (this.users[index] = { ...this.users[index], ...updateUserDto });
+    return this.userService.updateUser(id, updateUserDto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id', ParseUUIDPipe) id: string) {
-    const index = this.users.findIndex((user) => user.id === id);
-    this.users.splice(index, 1);
-
-    // this.users = this.users.filter((user) => user.id !== id);
+    this.userService.deleteUser(id);
   }
 }
---
+++ src/users/users.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserEntity } from './user.entity';
+import { createUserDto } from './dtos/create-user.dto';
+import { v4 as uuid } from 'uuid';
+import { updateUserDto } from './dtos/update-user.dto';
+
+@Injectable()
+export class UserService {
+  private users: UserEntity[] = [];
+
+  findUsers(): UserEntity[] {
+    return this.users;
+  }
+
+  findUserById(id: string): UserEntity {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) throw new NotFoundException('user not found, please try again ');
+
+    return user;
+  }
+
+  createUser(createUserDto: createUserDto): UserEntity {
+    const newUser = {
+      ...createUserDto,
+      id: uuid(),
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  updateUser(id: string, updateUserDto: updateUserDto): UserEntity {
+    const index = this.users.findIndex((user) => user.id === id);
+    return (this.users[index] = { ...this.users[index], ...updateUserDto });
+  }
+
+  deleteUser(id: string): void {
+    const index = this.users.findIndex((user) => user.id === id);
+    this.users.splice(index, 1);
+    // this.users = this.users.filter((user) => user.id !== id);
+  }
+}
